d34d6d188e325ea8d10d6104124a927bf4375c27
Jarosław Leśniak <jarek9244@gmail.com>
2020-04-17 22:50:14 +0200

    Added categories

--- app/src/main/java/edu/psm/budzetdomowy/TransactionManager.java
+++ app/src/main/java/edu/psm/budzetdomowy/TransactionManager.java
@@ -9,6 +9,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -28,10 +29,14 @@ import edu.psm.budzetdomowy.utils.Transaction;
 
 public class TransactionManager extends AppCompatActivity implements View.OnClickListener {
 
-    String defaultCategory;
+    boolean isFirstSelect = true;
+
+    String selectedCategory;
     int transactionType;
 
     TextView dateTextView;
+    Button submitButton;
+    Spinner categoriesList;
 
     final Calendar calendar = Calendar.getInstance();
 
@@ -50,7 +55,7 @@ public class TransactionManager extends AppCompatActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_manager);
 
-        defaultCategory = getIntent().getStringExtra("category");
+        selectedCategory = getIntent().getStringExtra("category");
         transactionType = getIntent().getIntExtra("type", Transaction.EXPENSE);
 
         ActionBar actionBar = getSupportActionBar();
@@ -58,10 +63,19 @@ public class TransactionManager extends AppCompatActivity implements View.OnClic
         actionBar.setTitle(transactionType == Transaction.INCOME ? "Dodaj przychód" : "Dodaj wydatek");
 
         dateTextView = (TextView) findViewById(R.id.dateTextView);
+        submitButton = (Button) findViewById(R.id.submitButton);
+        categoriesList = (Spinner) findViewById(R.id.categoriesList);
+
+
+        if(selectedCategory != null) {
+            submitButton.setText("Dodaj do '" + selectedCategory + "'");
+        }
 
         dateTextView.setOnClickListener(this);
+        submitButton.setOnClickListener(this);
 
         setDate();
+        prepareSpinner();
     }
 
     @Override
@@ -86,11 +100,55 @@ public class TransactionManager extends AppCompatActivity implements View.OnClic
                         calendar.get(Calendar.DAY_OF_MONTH)
                 ).show();
             break;
+            case R.id.submitButton:
+                if(selectedCategory == null) {
+                    categoriesList.performClick();
+                }
+                break;
         }
     }
 
     void setDate() {
         DateFormat dateFormat = new SimpleDateFormat("EEEE, d MMMM", new Locale("pl", "PL"));
         dateTextView.setText(dateFormat.format(calendar.getTime()));
+    }
+
+    void prepareSpinner() {
+        List<String> list = new ArrayList<>();
+
+        list.add(Category.SPORT);
+        list.add(Category.HOME);
+        list.add(Category.ENTERTAINMENT);
+        list.add(Category.FOOD);
+        list.add(Category.COSMETICS);
+        list.add(Category.MEDIA);
+        list.add(Category.TAXI);
+        list.add(Category.SHOPPING);
+        list.add(Category.ANIMALS);
+        list.add(Category.HEALTH);
+        list.add(Category.TRANSPORT);
+        list.add(Category.CLOTHES);
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categoriesList.setAdapter(dataAdapter);
+
+        categoriesList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View arg1, int pos, long arg3) {
+                String selectedItem = parent.getSelectedItem().toString();
+
+                if(!isFirstSelect) {
+                    selectedCategory = selectedItem;
+                    submitButton.setText("Dodaj do '" + selectedCategory + "'");
+                }
+
+                isFirstSelect = false;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
