75ea335ed8c11275e9ccd227606fbb2c8c351495
Jarosław Leśniak <jarek9244@gmail.com>
2020-05-09 22:17:07 +0200

    Clean up

--- app/src/main/java/edu/psm/budzetdomowy/transactionList.java
+++ app/src/main/java/edu/psm/budzetdomowy/transactionList.java
@@ -4,23 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
+import java.util.List;
+
 import edu.psm.budzetdomowy.R;
+import edu.psm.budzetdomowy.src.CBalanceSummary;
 
 public class transactionList extends BottomSheetDialogFragment {
-
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_transaction_list, container, false);
-
-
 
         return v;
     }
