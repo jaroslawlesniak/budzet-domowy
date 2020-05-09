cf0b65c080bc89fa84bc8efd344e0665f5a29676
Jarosław Leśniak <jarek9244@gmail.com>
2020-05-09 12:13:43 +0200

    Fixed error while loading new category activity

--- app/src/main/java/edu/psm/budzetdomowy/homepage.java
+++ app/src/main/java/edu/psm/budzetdomowy/homepage.java
@@ -21,8 +21,6 @@ public class homepage extends AppCompatActivity implements View.OnClickListener 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
-//        getSupportActionBar().hide();
-
         findViewById(R.id.btn1).setOnClickListener(this);
         findViewById(R.id.btn2).setOnClickListener(this);
         findViewById(R.id.btn3).setOnClickListener(this);
