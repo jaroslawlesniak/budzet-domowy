eac624642a4c3952bc71ab6f98d0ae4128622133
Jarosław Leśniak <jarek9244@gmail.com>
2020-05-09 12:47:31 +0200

    Closing Drawer after selecting menu item

---
+++ app/src/main/java/edu/psm/budzetdomowy/utils/SummaryInterval.java
@@ -0,0 +1,10 @@
+package edu.psm.budzetdomowy.utils;
+
+public class SummaryInterval {
+    public static final int DAY = 0;
+    public static final int WEEK = 1;
+    public static final int MONTH = 2;
+    public static final int YEAR = 3;
+    public static final int ALL = 4;
+    public static final int CUSTOM_DAY = 5;
+}
